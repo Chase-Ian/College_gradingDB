ea0a1e7f960ddadbc7f7c4a80233745faf28ed63
enderplayz05 <chaseian05@gmail.com>
2026-02-10 20:45:09 +0800

    added initial css

--- college_gradingDB/src/main.js
+++ college_gradingDB/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css' // If you're using Tailwind or custom CSS
+import './css/style.css' // If you're using Tailwind or custom CSS
 import App from './App.vue'
 
 createApp(App).mount('#app')
