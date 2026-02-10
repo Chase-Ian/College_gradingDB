9116f2e8ff505fc6cc73dce8831177fd4d5ce44a
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-02-10 16:00:07 +0000

    updated supabase url and keys

--- college_gradingDB/src/supabase.js
+++ college_gradingDB/src/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://vbpqqdcbzkmeifllquhc.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZicHFxZGNiemttZWlmbGxxdWhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MTQyNDcsImV4cCI6MjA4NjI5MDI0N30.GWj1d1Wk7pgrNeTR6O4KRcSIyT6P3nUJOIKWSznlMcs'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
